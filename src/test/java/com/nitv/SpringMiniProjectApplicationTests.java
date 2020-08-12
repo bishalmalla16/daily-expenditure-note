ac06d4cc020627d367fa12e12c45a4575b17256e
Bishal Malla <bishalmalla16@gmail.com>
2020-08-12 12:51:45 +0545

    spring web application first stage

---
+++ src/test/java/com/nitv/SpringMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
